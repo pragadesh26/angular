401c2d59014096676a562f5bce5c805ee3f1d2f2
devteam@balkansitsolutions.com <devteam@balkansitsolutions.com>
2025-07-08 12:06:55 +0530

    Initial commit with Angular and Laravel projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'login-front';
+  email = '';
+  password = '';
+  loginSuccess = false;
+  loggedInUser: any;
+
+  constructor(private http: HttpClient) {}
+
+  login() {
+    this.http.post('http://127.0.0.1:8000/api/login', {
+      email: this.email,
+      password: this.password
+    }).subscribe({
+      next: (res: any) => {
+        this.loggedInUser = res.user;
+        this.loginSuccess = true;
+      },
+      error: () => {
+        this.loginSuccess = false;
+      }
+    });
+  }
 }
